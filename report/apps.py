45e06e69625a9c54fdc35c3518c027a8ed54e0ad
Cameron <pluckthechicken@hotmail.co.uk>
2019-11-30 13:50:31 +1000

    Added appconfig to run plot_history on startup

--- report/apps.py
+++ report/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from .views import plot_history
 
 class ReportConfig(AppConfig):
     name = 'report'
+    verbose_name = 'Portfolio report'
+    def ready(self):
+        plot_history()
