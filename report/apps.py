08e65315cda849d6d893f533a1d568595887be0d
Cameron <pluckthechicken@hotmail.co.uk>
2019-11-30 14:10:11 +1000

    Debug appconfig

--- report/apps.py
+++ report/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-from .views import plot_history
 
 class ReportConfig(AppConfig):
     name = 'report'
     verbose_name = 'Portfolio report'
     def ready(self):
+        from .views import plot_history
         plot_history()
