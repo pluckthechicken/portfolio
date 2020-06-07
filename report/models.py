9ab6357deef4e6d6b9f447b337dc725fa68de1b5
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2020-06-07 14:20:20 +1000

    Adapted to allow staggered start dates on positions.

--- report/models.py
+++ report/models.py
@@ -1,20 +1,160 @@
+"""Store each position as a historical data object.
+
+Contains historical share prices which update each time the model is queried.
+This improves efficiency since the data are only ever requested once.
+All prices reflect daily close.
+"""
+
+import json
+from datetime import date, datetime
+import pandas_datareader as pdr
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
-class Historical(models.Model):
-    """ Store historical stock data """
+COLOURS = [
+    'orange',   # EVX
+    'red',      # GWPH
+    'green',    # QCLN
+    'blue',
+    'purple',
+    'grey',
+    'yellow',
+    'maroon',
+    'cyan',
+    'brown',
+    'black',
+]
+
+
+class Position(models.Model):
+    """Store historical share price data."""
+
     date_updated = models.DateTimeField(auto_now=True)
-    stock = models.CharField(max_length=15)
+    stock_code = models.CharField(max_length=15)
     series_x = ArrayField(models.DateField())
     series_y = ArrayField(models.FloatField())
     buy_price = models.FloatField()
     buy_qty = models.IntegerField()
     current = models.FloatField()
+    holding = models.FloatField()
 
-    def update(self, series_x, series_y, current):
-        """ Update the model with today's data """
-        self.series_x = series_x
-        self.series_y = series_y
-        self.current = current
+    @classmethod
+    def create(cls, stock_code, buy_date, buy_price, buy_qty):
+        """Fetch data and create new stock object."""
+        dfs = pdr.get_data_yahoo(
+            stock_code,
+            start=buy_date,
+            end=date.today()
+        )
+        close = dfs['Close']
+        PL = (close / buy_price - 1).round(2)
+        return cls.objects.create(
+            stock_code=stock_code,
+            buy_price=buy_price,
+            buy_qty=buy_qty,
+            series_x=list(PL.index),
+            series_y=list(PL),
+            current=close.iloc[-1],
+            holding=round(close.iloc[-1] * buy_qty, 2)
+        )
+
+    @classmethod
+    def get_all(cls):
+        """Update and return all stocks."""
+        positions = cls.objects.all()
+        for p in positions:
+            p.update()
+        return positions
+
+    @classmethod
+    def render_report(cls):
+        """Render data for portfolio report."""
+        def usd_fmt(value):
+            """Comma-separate values over $10 and return string formatted."""
+            if value < 10000:
+                return '$%.2f' % value
+            x = '%.2f' % value
+            x, y = x.split('.')
+            return '$%s,%s.%s' % (x[:-3], x[-3:], y)
+
+        data = {}
+        total_pl = 0
+        init_holdings = 0
+        total_holdings = sum(cls.objects.all().values_list(
+            "holding", flat=True))
+
+        for p in cls.objects.all():
+            print('Loading data for %s' % p.stock_code)
+            pl = (p.current - p.buy_price) / p.buy_price
+            pl_pc = 100 * pl
+            init_holding = p.buy_qty * p.buy_price
+            init_holdings += init_holding
+            pl_usd = init_holding * pl
+            total_pl += (pl_usd * p.holding / total_holdings)
+            data[p.stock_code] = [
+                '%.2f' % p.buy_price,
+                '%s' % p.buy_qty,
+                '%.2f' % p.current,
+                usd_fmt(p.holding),
+                usd_fmt(pl_usd),
+                '%.2f %%' % pl_pc,
+                'plus' if pl_pc > 0 else 'minus',
+            ]
+
+        data['Total'] = [
+            usd_fmt(init_holdings),
+            '',
+            '',
+            usd_fmt(total_holdings),
+            usd_fmt(total_pl),
+            '%.2f %%' % (100 * total_pl / init_holdings),
+            'total',
+        ]
+
+        return data
+
+    @classmethod
+    def fetch_plot_json(cls):
+        """Fetch stock P/L data and format for plotly."""
+        positions = cls.get_all().order_by('stock_code')
+        colours = COLOURS[:len(positions)]
+        data = []
+
+        for position, clr in zip(positions, colours):
+            series = {
+                'type': 'scatter',
+                'mode': 'lines',
+                'line': {'color': clr}
+            }
+            series['name'] = position.stock_code
+            series['x'] = [
+                datetime.strftime(x, '%Y-%m-%d') for x in position.series_x
+            ]
+            series['y'] = position.series_y
+            data.append(series)
+        return json.dumps(data)
+
+    def update(self):
+        """Update the model with today's data."""
+        def fetch_close():
+            date_from = self.series_x[-1]
+            dfs = pdr.get_data_yahoo(
+                self.stock_code,
+                start=date_from,
+                end=date.today()
+            )
+            # Remove duplicate dates (causes a bug)
+            dfs = dfs.loc[~dfs.index.duplicated(keep='first')]
+            if len(dfs):
+                return dfs['Close']
+
+        close = fetch_close()
+        if close is None:
+            return
+        PL = (close / self.buy_price - 1).round(2)
+        self.series_x += list(PL.index)
+        self.series_y += list(PL)
+        self.current = close.iloc[-1]
+        self.holding = round(self.current * self.buy_qty, 2)
         self.save()
--- report/views.py
+++ report/views.py
@@ -4,114 +4,14 @@ Also shows an update view which updates static content and then redirects back
 to the homepage.
 """
 
-import os
-import json
-import pandas_datareader as pdr
-from datetime import date, datetime
 from django.shortcuts import render
-from .models import Historical
-
-
-start = datetime(2019, 10, 10)
-end = datetime.today()
+from .models import Position
 
 
 def home(request):
     """Show historical trend of stocks as graphs and tables."""
-    def usd_fmt(value):
-        """Comma-separate values over $10 and return string formatted."""
-        if value < 10000:
-            return '$%.2f' % value
-        x = '%.2f' % value
-        x, y = x.split('.')
-        return '$%s,%s.%s' % (x[:-3], x[-3:], y)
-
-    plot_data = fetch_plot_json()
-
-    data = {}
-    init_holdings = 0
-    total_holding = 0
-    total_pl = 0
-
-    for h in Historical.objects.all():
-        print('Loading data for %s' % h.stock)
-        current = h.current
-        pl_pc = 100 * (current - h.buy_price) / h.buy_price
-        holding = h.buy_qty * current
-        init_holding = h.buy_qty * h.buy_price
-        init_holdings += init_holding
-        pl_usd = init_holding * (pl_pc / 100)
-        total_holding += holding
-        total_pl += pl_usd
-        data[h.stock] = [
-            '%.2f' % h.buy_price,
-            '%s' % h.buy_qty,
-            '%.2f' % current,
-            usd_fmt(holding),
-            usd_fmt(pl_usd),
-            '%.2f %%' % pl_pc,
-            'plus' if pl_pc > 0 else 'minus',
-        ]
-
-    data['Total'] = [
-        usd_fmt(init_holdings),
-        '',
-        '',
-        usd_fmt(total_holding),
-        usd_fmt(total_pl),
-        '%.2f %%' % (100 * total_pl / init_holdings),
-        'total',
-    ]
-
-    pl = total_holding / init_holdings
-    yearfrac = 365 / (end - start).days
-
     return render(request, 'report/index.html', {
-        'stocks': data,
-        'plotData': plot_data,
-        'projected': usd_fmt(init_holdings * (pl ** yearfrac)),
-        'yearfrac': '%.0f' % (100 / yearfrac),
+        'stocks': Position.render_report(),
+        'plotData': Position.fetch_plot_json(),
         }
     )
-
-
-def fetch_plot_json():
-    """Fetch stock P/L data and format for plotly."""
-    def fetch_data(stocks):
-        for stock in stocks:
-            dfs = pdr.get_data_yahoo(stock.stock, start=start, end=end)
-            close = dfs['Close']
-            pl = close / close.iloc[0] - 1
-            stock.update(list(pl.index), list(pl), close.iloc[-1])
-
-    stocks = Historical.objects.all().order_by('stock')
-    data = []
-
-    colours = [
-        'brown',   # EVX
-        'red',     # GWPH
-        'blue',    # IXN
-        'orange',  # MOAT
-        'green',   # QCLN
-        'purple',  # SPY
-        'grey',
-        'yellow',
-        'maroon',
-        'cyan',
-        'black',
-    ][:len(stocks)]
-
-    if stocks[0].series_x[-1] < date.today():
-        # Hasn't been updated today
-        fetch_data(stocks)
-
-    for stock, clr in zip(stocks, colours):
-        series = {'type': 'scatter', 'mode': 'lines', 'line': {'color': clr}}
-        series['name'] = stock.stock
-        series['x'] = [
-            datetime.strftime(x, '%Y-%m-%d') for x in stock.series_x
-        ]
-        series['y'] = stock.series_y
-        data.append(series)
-
-    return json.dumps(data)
